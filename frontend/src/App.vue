4156dd3611c8b4972fee2587491d87d420de665b
Ben Burwood <ben.burwood@streetwave.co>
2025-10-13 22:03:04 +0100

    Initialise Frontend Project

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Certificate Status Page</h1>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ frontend/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+
+export const SERVER_URL = 'http://localhost:8080'
+
+createApp(App).mount('#app')
