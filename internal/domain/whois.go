69ea6cb1597bc2cd0075ebd51893c2d8cdeb843e
Ben Burwood <ben.burwood@streetwave.co>
2025-11-21 14:49:16 +0000

    Add WhoisDetails to Handler

--- internal/domain/whois.go
+++ internal/domain/whois.go
@@ -1,11 +1,28 @@
 package domain
 
 import (
+	"time"
+
 	"github.com/likexian/whois"
 	whoisparser "github.com/likexian/whois-parser"
 )
 
-func WhoisForDomain(domain Domain) (*whoisparser.WhoisInfo, error) {
+type WhoisDetails struct {
+	Registrar      string
+	NameServers    []string
+	ExpirationDate time.Time
+}
+
+func NewWhoisDetails(whoisInfo *whoisparser.WhoisInfo) *WhoisDetails {
+	return &WhoisDetails{
+		Registrar:      whoisInfo.Registrar.Name,
+		NameServers:    whoisInfo.Domain.NameServers,
+		ExpirationDate: *whoisInfo.Domain.ExpirationDateInTime,
+	}
+}
+
+// WhoisForDomain retrieves the whois information for a domain.
+func WhoisForDomain(domain Domain) (*WhoisDetails, error) {
 	whoisRaw, err := whois.Whois(string(domain.stripHttpPrefix()))
 	if err != nil {
 		return nil, err
@@ -16,5 +33,5 @@ func WhoisForDomain(domain Domain) (*whoisparser.WhoisInfo, error) {
 		return nil, err
 	}
 
-	return &whois, nil
+	return NewWhoisDetails(&whois), nil
 }
